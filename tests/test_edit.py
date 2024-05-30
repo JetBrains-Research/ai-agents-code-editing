868f02941d3336a2a10e78e56d89884a94929f7b
Alexander Kovrigin <alexander.kovrigin@jetbrains.com>
2024-05-30 14:47:22 +0200

    formatting

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -15,7 +15,9 @@ def test_process_edit():
     lines_to_edit = [2, 3, 5, 6, 8, 9]
     edited_code = process_edit(file, lines_to_edit, mock_edit_func)
 
-    expected_code = ("line 1\n<edited>line 2\nline 3</edited>\nline 4\n<edited>line 5\nline 6</edited>\nline "
-                     "7\n<edited>line 8\nline 9</edited>\nline 10")
+    expected_code = (
+        "line 1\n<edited>line 2\nline 3</edited>\nline 4\n<edited>line 5\nline 6</edited>\nline "
+        "7\n<edited>line 8\nline 9</edited>\nline 10"
+    )
 
     assert edited_code == expected_code
