33e6676fdda337fe0cd9cdd86844a86d0c92ca7c
Alexander Kovrigin <alexander.kovrigin@jetbrains.com>
2024-07-10 15:25:35 +0200

    fix aider.py

--- code_editing/agents/context_providers/aider/aider.py
+++ code_editing/agents/context_providers/aider/aider.py
@@ -1,3 +1,5 @@
+import tiktoken
+
 from code_editing.agents.context_providers.aider.repo_map import RepoMap, find_src_files
 from code_editing.agents.context_providers.context_provider import ContextProvider
 
@@ -7,10 +9,21 @@ class AiderRepoMap(ContextProvider):
         self.repo_path = repo_path
         self.data_path = data_path
 
+        def count_tokens(text, model="gpt-4o"):
+            # Get the appropriate encoding for the specified model
+            encoding = tiktoken.encoding_for_model(model)
+
+            # Encode the text using the encoding scheme
+            tokens = encoding.encode(text)
+
+            # Return the number of tokens
+            return len(tokens)
+
         self.rm = RepoMap(
             map_tokens=1024,
             root=repo_path,
-            token_count=lambda x: len(x.split()) // 4,
+            token_count=count_tokens,
+            # max_context_window=120000
         )
 
     def get_repo_map(self) -> str:
